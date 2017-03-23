a58748450d8f7e179634f2e0c0f3f6178cf7e4b6
Park Joonyoung <parkjydev@gmail.com>
2017-03-24 02:16:38 +0900

    Push done
    receives push notification when the article is updated.

--- ios/standup_native_webview/AppDelegate.m
+++ ios/standup_native_webview/AppDelegate.m
@@ -15,8 +15,10 @@
 
 @import UserNotifications;
 @import Firebase;
+@import FirebaseInstanceID;
+@import FirebaseMessaging;
 
-@interface AppDelegate () <UNUserNotificationCenterDelegate>
+@interface AppDelegate () <UNUserNotificationCenterDelegate, FIRMessagingDelegate>
 @end
 
 @implementation AppDelegate
@@ -27,65 +29,57 @@ NSString *const kGCMMessageIDKey = @"gcm.message_id";
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"standup_native_webview"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  
-  if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_9_x_Max) {
-    UIUserNotificationType allNotificationTypes =
-    (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
-    UIUserNotificationSettings *settings =
-    [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
-    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
-  } else {
-    // iOS 10 or later
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-    // For iOS 10 display notification (sent via APNS)
-    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
-    UNAuthorizationOptions authOptions =
-    UNAuthorizationOptionAlert
-    | UNAuthorizationOptionSound
-    | UNAuthorizationOptionBadge;
-    [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {
-    }];
-    
-    // For iOS 10 data message (sent via FCM)
-    [FIRMessaging messaging].remoteMessageDelegate = self;
-#endif
-  }
+
+  [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+  UNAuthorizationOptions authOptions =
+  UNAuthorizationOptionAlert
+  | UNAuthorizationOptionSound
+  | UNAuthorizationOptionBadge;
+  [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {
+  }];
+  // For iOS 10 data message (sent via FCM)
+  [FIRMessaging messaging].remoteMessageDelegate = self;
   
   [[UIApplication sharedApplication] registerForRemoteNotifications];
-  
+
   [FIRApp configure];
   
+  // Add observer for InstanceID token refresh callback.
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(tokenRefreshNotification:)
+                                               name:kFIRInstanceIDTokenRefreshNotification object:nil];
+  NSString *token = [[FIRInstanceID instanceID] token];
+
   return YES;
 }
 
 // Required to register for notifications
 - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
 {
+  NSString *token = [[FIRInstanceID instanceID] token];
+  NSLog(@"InstanceID token: %@", token);
+  
+  [[FIRMessaging messaging] subscribeToTopic:@"/topics/allDevice"];
+  NSLog(@"Subscribed to 'allDevice' topic");
   [RCTPushNotificationManager didRegisterUserNotificationSettings:notificationSettings];
 }
 
-
 // Required for the register event.
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-{
-  NSLog(@"%@", deviceToken);
-  [RCTPushNotificationManager didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  // With swizzling disabled you must set the APNs token here.
+  [[FIRInstanceID instanceID] setAPNSToken:deviceToken type:FIRInstanceIDAPNSTokenTypeSandbox];
+  NSLog(@"APNs token retrieved: %@", deviceToken);
 }
-
 
 // Required for the notification event. You must call the completion handler after handling the remote notification.
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
@@ -95,22 +89,21 @@ NSString *const kGCMMessageIDKey = @"gcm.message_id";
   if (userInfo[kGCMMessageIDKey]) {
     NSLog(@"Message ID: %@", userInfo[kGCMMessageIDKey]);
   }
-  
+
   // Print full message.
   NSLog(@"%@", userInfo);
-  
+
   completionHandler(UIBackgroundFetchResultNewData);
-  
+
   [RCTPushNotificationManager didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
 }
-
 
 // Required for the registrationError event.
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
 {
+  NSLog(@"Unable to register for remote notifications: %@", error);
   [RCTPushNotificationManager didFailToRegisterForRemoteNotificationsWithError:error];
 }
-
 
 // Required for the localNotification event.
 - (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
@@ -118,6 +111,48 @@ NSString *const kGCMMessageIDKey = @"gcm.message_id";
   [RCTPushNotificationManager didReceiveLocalNotification:notification];
 }
 
+- (void)tokenRefreshNotification:(NSNotification *)notification {
+  // Note that this callback will be fired everytime a new token is generated, including the first
+  // time. So if you need to retrieve the token as soon as it is available this is where that
+  // should be done.
+  NSString *refreshedToken = [[FIRInstanceID instanceID] token];
+  NSLog(@"InstanceID token: %@", refreshedToken);
+  
+  // Connect to FCM since connection may have failed when attempted before having a token.
+  [self connectToFcm];
+  
+  // TODO: If necessary send token to application server.
+}
 
+- (void)connectToFcm {
+  // Won't connect since there is no token
+  if (![[FIRInstanceID instanceID] token]) {
+    return;
+  }
+  
+  // Disconnect previous FCM connection if it exists.
+  [[FIRMessaging messaging] disconnect];
+  
+  [[FIRMessaging messaging] connectWithCompletion:^(NSError * _Nullable error) {
+    if (error != nil) {
+      NSLog(@"Unable to connect to FCM. %@", error);
+    } else {
+      NSLog(@"Connected to FCM.");
+    }
+  }];
+}
+
+// [START connect_on_active]
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  [self connectToFcm];
+}
+// [END connect_on_active]
+
+// [START disconnect_from_fcm]
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+  [[FIRMessaging messaging] disconnect];
+  NSLog(@"Disconnected from FCM");
+}
+// [END disconnect_from_fcm]
 
 @end
